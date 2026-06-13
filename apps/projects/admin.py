58400bf57dfb18c1d57a1169a427f818b222242b
talhawkk <talhawkk@gmail.com>
2026-06-14 03:18:09 +0500

    bulk upload feature

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django import forms
+from django.db.models import Max
 from .models import Technology, Project, ProjectImage
 
+
+class ProjectAdminForm(forms.ModelForm):
+    gallery_folder = forms.FileField(
+        widget=forms.ClearableFileInput(attrs={'multiple': True, 'webkitdirectory': True, 'directory': True}),
+        required=False,
+        label="Upload Gallery Folder",
+        help_text="Select a folder to upload all images inside it. The sequence will be based on file names. Images will be added to the gallery below."
+    )
+    gallery_multiple = forms.FileField(
+        widget=forms.ClearableFileInput(attrs={'multiple': True}),
+        required=False,
+        label="Upload Multiple Images",
+        help_text="Select multiple individual images to upload at once."
+    )
+
+    class Meta:
+        model = Project
+        fields = '__all__'
 
 class ProjectImageInline(admin.TabularInline):
     model = ProjectImage
@@ -17,6 +37,7 @@ class TechnologyAdmin(admin.ModelAdmin):
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
+    form = ProjectAdminForm
     list_display = ('title', 'tagline', 'is_featured', 'order', 'created_at')
     list_filter = ('is_featured', 'technologies')
     list_editable = ('is_featured', 'order')
@@ -35,6 +56,10 @@ class ProjectAdmin(admin.ModelAdmin):
         ('Media', {
             'fields': ('featured_image', 'thumbnail')
         }),
+        ('Bulk Gallery Upload', {
+            'fields': ('gallery_folder', 'gallery_multiple'),
+            'classes': ('collapse',)
+        }),
         ('Case Study', {
             'fields': ('detailed_description', 'challenge', 'solution', 'results'),
             'classes': ('collapse',)
@@ -43,3 +68,29 @@ class ProjectAdmin(admin.ModelAdmin):
             'fields': ('is_featured', 'order')
         }),
     )
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        
+        # Get current max order
+        current_max_order = obj.images.aggregate(Max('order'))['order__max']
+        if current_max_order is None:
+            current_max_order = -1
+
+        # Retrieve files
+        files = request.FILES.getlist('gallery_multiple')
+        folder_files = request.FILES.getlist('gallery_folder')
+
+        # Combine and sort to maintain sequence
+        all_files = files + folder_files
+        all_files.sort(key=lambda x: x.name)
+
+        # Create ProjectImage objects
+        for f in all_files:
+            current_max_order += 1
+            ProjectImage.objects.create(
+                project=obj,
+                image=f,
+                order=current_max_order,
+                caption=f.name
+            )
